0ed0f5e548e06b1eeb37489e8549548e2950e8fd
prathambongale <prathambongale@hotmail.com>
2022-01-11 18:22:01 +0530

    adding complex decision

--- camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/b/MyFileRouter.java
+++ camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/b/MyFileRouter.java
@@ -1,10 +1,21 @@
 package com.in28minutes.microservices.camelmicroservicea.routes.b;
 
+import java.util.Map;
+
+import org.apache.camel.Body;
+import org.apache.camel.ExchangeProperties;
+import org.apache.camel.Headers;
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyFileRouter extends RouteBuilder{
+
+    @Autowired
+    private DeciderBean deciderBean;
 
     @Override
     public void configure() throws Exception {
@@ -14,14 +25,13 @@ public class MyFileRouter extends RouteBuilder{
         .choice()
             .when(simple("${file:ext} ends with 'xml'"))
                 .log("XML FILE")
-            .when(simple("${body} contains 'USD'"))
+            //.when(simple("${body} contains 'USD'"))
+            .when(method(deciderBean))
                 .log("NOT and XML FILE but contains USD")
             .otherwise()
                 .log("NOT and XMl FILE")
         .end()
-        .log("${body}")
-        .log("${messageHistory} ${file:absolute.path}")
-        .to("direct://log-file-values")
+        //.to("direct://log-file-values")
         .to("file:files/output");
 
 
@@ -34,4 +44,18 @@ public class MyFileRouter extends RouteBuilder{
         .log("${routeId} ${camelId} ${body}");
     }
     
+}
+
+
+@Component
+class DeciderBean {
+    Logger logger = LoggerFactory.getLogger(DeciderBean.class);
+
+    public boolean isThisConditionMet(@Body String body, 
+        @Headers Map<String, String> headers, 
+        @ExchangeProperties Map<String, String> exchangeProperties) {
+        
+        logger.info("DeciderBean {} {} {}", body, headers, exchangeProperties);
+        return true;
+    }
 }
